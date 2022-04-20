a4fc5c37b99190eca5769730e9f6b887ac96611c
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-20 07:47:35 +0530

    Added : readme file

---
+++ addressBookSystem/src/com/bridgelabz/addressbooksystem/AddressBook.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.addressbooksystem;
+/*
+ * Creating contact properties
+ * @author: Sadanand Pandey
+ */
+public class AddressBook {
+
+	public static void main(String[] args) {
+		
+		String firstName, lastName, email;
+		long phoneNumber;
+		String city, state;
+		long zip;
+		System.out.println("=====================================================================");
+		System.out.println("-----------------Welcome to address book Program --------------------");
+		System.out.println("=====================================================================");
+		System.out.println();
+	}
+
+}
