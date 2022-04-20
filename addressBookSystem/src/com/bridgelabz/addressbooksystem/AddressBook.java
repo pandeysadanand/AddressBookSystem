8536aa43190dc17fbd74baa7159ae15c9fab5b0c
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-20 16:23:13 +0530

    Added: Adding new contact to address book

--- addressBookSystem/src/com/bridgelabz/addressbooksystem/AddressBook.java
+++ addressBookSystem/src/com/bridgelabz/addressbooksystem/AddressBook.java
@@ -1,4 +1,7 @@
 package com.bridgelabz.addressbooksystem;
+
+import java.util.Scanner;
+
 /*
  * Creating contact properties
  * @author: Sadanand Pandey
@@ -6,15 +9,28 @@ package com.bridgelabz.addressbooksystem;
 public class AddressBook {
 
 	public static void main(String[] args) {
-		
-		String firstName, lastName, email;
-		long phoneNumber;
-		String city, state;
-		long zip;
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println("=====================================================================");
 		System.out.println("-----------------Welcome to address book Program --------------------");
 		System.out.println("=====================================================================");
 		System.out.println();
+		Address address = new Address();
+		System.out.println("Enter first name: ");
+		address.setFirstName(sc.next());
+		System.out.println("Enter last name: ");
+		address.setLastName(sc.next());
+		System.out.println("Enter email: ");
+		address.setEmail(sc.next());
+		System.out.println("Enter phone number: ");
+		address.setPhoneNumber(sc.nextLong());
+		System.out.println("Enter city: ");
+		address.setCity(sc.next());
+		System.out.println("Enter state: ");
+		address.setState(sc.next());
+		System.out.println("Enter zip code : ");
+		address.setZip(sc.nextLong());
+		
+		System.out.println(address.toString());
 	}
-
 }
